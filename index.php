833e73f1d6378ef49a29511820c684c9febafeec
sepatimaria <septimh695@gmail.com>
2025-11-21 10:00:33 +0700

    style css

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
-<html lang="en"
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Sample HTML Page</title>
-        <meta http-equiv="X-UA-Compatible" content="ie"
-    </head>
-    <body>
-        <h1>Hello World!</h1>
-        <p>lorem ipsum</p>
-    </body>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample HTML Page</title>
+    <meta http-equiv="X-UA-Compatible" content="ie">
+
+    <!-- Tambahan CSS: ubah font -->
+    <style>
+        body {
+            font-family: "Poppins", sans-serif;
+        }
+    </style>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>lorem ipsum</p>
+</body>
+</html>
