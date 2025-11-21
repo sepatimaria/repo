a4280b933adeec5578a96a41fd5974a103c006df
sepatimaria <septimh695@gmail.com>
2025-11-21 09:41:00 +0700

    Menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en"
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sample HTML Page</title>
+        <meta http-equiv="X-UA-Compatible" content="ie"
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <p>lorem ipsum</p>
+    </body>
+<html>
