0965e3498a8f472cc3ee8dd43865c3a71183d2cd
sepatimaria <septimh695@gmail.com>
2025-11-28 12:26:54 +0700

    Add index.php and style.css from Septi repo

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Git Remote</title>
+    </head>
+    <body>
+        <h1>HELLO WORLD!</h1>
+        <p>latihan git</p>
+        <p>git push</p>
+        <p>git pull</p>
+    </body>
+</html>
